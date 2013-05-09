8f7c384471f0ccdf4cd4ea6ccc225a99c01f75c3
Dmitry <dmitry@home-nb-linux.(none)>
2013-05-09 17:30:54 +0300

    Private Site application with BDD enviroment

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MyPrivateSite::Application.config.secret_token = '0171bc7fc16de70d45b42637d54465d42437fd35680e7e54cd8eaf37407709231b8511f63933f3176f972c7ca962599bea594447987e11b6a2f5a4982010fb94'
